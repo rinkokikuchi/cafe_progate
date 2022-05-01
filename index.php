bd804933a6179116cbc81fd79eeafa0703569afa
sugawara-nana <sugawara.77lopi@gmail.com>
2022-05-01 21:48:39 +0900

    ５月１日

--- index.php
+++ index.php
@@ -6,11 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>html.test</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <!-- link relはタイトルフォントのデータ-->
 </head>
 <body>
     <header>
         <div class = "title">
-            <p>cafe progate</p>
+            <p class = "title_name">cafe progate</p>
+            <h5>メニュー４品</h5>
         </div>
 
 
@@ -19,6 +24,51 @@
 
 
     </header>
+
+    <div class = "main">
+        <div class = "menu">
+            <img src = "img/juice.jpg">
+            <h3>JUICE</h3>
+            <h5>アイス</h5>
+            <p>¥648（税込み）</p>
+            <input>
+            <p>個</p>
+        </div>
+        <div class = "menu">
+            <img src = "img/coffee.jpg">
+            <h3>COFFEE</h3>
+            <h5>ホット</h5>
+            <p>¥540（税込み）</p>
+            <input>
+            <p>個</p>
+        </div>
+        <div class = "menu">
+            <img src = "img/curry.jpg">
+            <h3>CURRY</h3>
+            <img src = "img/chilli.png">
+            <img src = "img/chilli.png">
+            <img src = "img/chilli.png">
+            <p>¥972（税込み）</p>
+            <input>
+            <p>個</p>
+        </div>
+        <div class = "menu">
+            <img src = "img/pasta.jpg">
+            <h3>PASTA</h3>
+            <img src = "img/chilli.png">
+            <img src = "img/chilli.png">
+            <p>¥1296（税込み）</p>
+            <input>
+            <p>個</p>
+        </div>
+        <div class = "order_btn">
+        <input type = "submit" value = "注文する" class = "btn">
+        </div>
+
+
+
+
+    </div>
 
 
 
