65f45047484281623b37641773d7c74799a85333
Nana.S <sugawara.77lopi@gmail.com>
2022-05-08 20:36:10 +0900

    インデントきれいにした

--- index.php
+++ index.php
@@ -1,76 +1,64 @@
 <DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>html.test</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-    <!-- link relはタイトルフォントのデータ-->
-</head>
-<body>
-    <header>
-        <div class = "title">
-            <h2 class = "title_name">Cafe Progate</h2>
-            <h6>メニュー４品</h6>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>html.test</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+        <!-- link relはタイトルフォントのデータ-->
+    </head>
+    <body>
+        <header>
+            <div class = "title">
+                <h2 class = "title_name">Cafe Progate</h2>
+                <h6>メニュー４品</h6>
+            </div>
+
+        </header>
+
+        <div class = "main">
+            <div class = "menu">
+                <img src = "img/juice.jpg">
+                <h3>JUICE</h3>
+                <h5>アイス</h5>
+                <p>¥648（税込み）</p>
+                <input>
+                <p>個</p>
+            </div>
+            <div class = "menu">
+                <img src = "img/coffee.jpg">
+                <h3>COFFEE</h3>
+                <h5>ホット</h5>
+                <p>¥540（税込み）</p>
+                <input>
+                <p>個</p>
+            </div>
+            <div class = "menu">
+                <img src = "img/curry.jpg">
+                <h3>CURRY</h3>
+                <img src = "img/chilli.png">
+                <img src = "img/chilli.png">
+                <img src = "img/chilli.png">
+                <p>¥972（税込み）</p>
+                <input>
+                <p>個</p>
+            </div>
+            <div class = "menu">
+                <img src = "img/pasta.jpg">
+                <h3>PASTA</h3>
+                <img src = "img/chilli.png">
+                <img src = "img/chilli.png">
+                <p>¥1296（税込み）</p>
+                <input>
+                <p>個</p>
+            </div>
+            <div class = "order_btn">
+            <input type = "submit" value = "注文する" class = "btn">
+            </div>
         </div>
-
-    </header>
-
-    <div class = "main">
-        <div class = "menu">
-            <img src = "img/juice.jpg">
-            <h3>JUICE</h3>
-            <h5>アイス</h5>
-            <p>¥648（税込み）</p>
-            <input>
-            <p>個</p>
-        </div>
-        <div class = "menu">
-            <img src = "img/coffee.jpg">
-            <h3>COFFEE</h3>
-            <h5>ホット</h5>
-            <p>¥540（税込み）</p>
-            <input>
-            <p>個</p>
-        </div>
-        <div class = "menu">
-            <img src = "img/curry.jpg">
-            <h3>CURRY</h3>
-            <img src = "img/chilli.png">
-            <img src = "img/chilli.png">
-            <img src = "img/chilli.png">
-            <p>¥972（税込み）</p>
-            <input>
-            <p>個</p>
-        </div>
-        <div class = "menu">
-            <img src = "img/pasta.jpg">
-            <h3>PASTA</h3>
-            <img src = "img/chilli.png">
-            <img src = "img/chilli.png">
-            <p>¥1296（税込み）</p>
-            <input>
-            <p>個</p>
-        </div>
-        <div class = "order_btn">
-        <input type = "submit" value = "注文する" class = "btn">
-        </div>
-
-
-
-
-    </div>
-
-
-
-
-
-
-
-</body>
-
+    </body>
 </html>
