c57d17cb81bfbc455f647cd569cf8dd7031f1cb7
Nana.S <sugawara.77lopi@gmail.com>
2022-06-19 20:17:41 +0900

    トップ完成

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
@@ -8,7 +8,7 @@
         <title>cafe_progate</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Pacifico&display=swap" rel="stylesheet">
         <!-- link relはタイトルフォントのデータ-->
     </head>
     <body>
@@ -23,43 +23,50 @@
         <div class = "main">
             <div class = "menu_juice">
                 <img src = "img/juice.jpg" class = "menu_jpg">
-                <h5>JUICE</h5>
+                <h4>JUICE</h4>
                 <h6>アイス</h6>
-                <p>¥648（税込）</p>
-                <input type ="text" class ="num" value ="0">
-                <p>個</p>
+                <p class = "price">¥648（税込）</p>
+                <div class="quantity">
+                    <input type ="text" class ="num" value ="0">
+                    <p >個</p>
+                </div>
             </div>
             <div class = "menu_coffee">
                 <img src = "img/coffee.jpg" class = "menu_jpg">
-                <h5>COFFEE</h5>
+                <h4>COFFEE</h4>
                 <h6>ホット</h6>
-                <p>¥540（税込）</p>
-                <input type ="text" class ="num" value ="0">
-                <p>個</p>
+                <p class = "price">¥540（税込）</p>
+                <div class="quantity">
+                    <input type ="text" class ="num" value ="0">
+                    <p >個</p>
+                </div>
             </div>
             <div class = "menu_curry">
                 <img src = "img/curry.jpg" class = "menu_jpg">
-                <h5>CURRY</h5>
+                <h4>CURRY</h4>
                 <img src = "img/chilli.png" class="chilli">
                 <img src = "img/chilli.png" class="chilli">
                 <img src = "img/chilli.png" class="chilli">
-                <p>¥972（税込）</p>
-                <input type ="text" class ="num" value ="0">
-                <p>個</p>
+                <p class = "price">¥972（税込）</p>
+                <div class="quantity">
+                    <input type ="text" class ="num" value ="0">
+                    <p >個</p>
+                </div>
             </div>
             <div class = "menu_pasta">
                 <img src = "img/pasta.jpg" class = "menu_jpg">
-                <h5>PASTA</h5>
+                <h4>PASTA</h4>
                 <img src = "img/chilli.png" class="chilli">
-                <img src = "img/chilli.png" class="chilli">
-                <p>¥1296（税込）</p>
-                <input type ="text" class ="num" value ="0">
-                <p>個</p>
+                <p class = "price">¥1296（税込）</p>
+                <div class="quantity">
+                    <input type ="text" class ="num" value ="0">
+                    <p >個</p>
+                </div>
             </div>
         </div>
         <div class = "menus">
             <div class = "order_btn">
-            <input type = "submit" value = "注文する" class = "btn">
+            <button  type = "submit" class = "btn">注文する</button>
         </div>
     </body>
 </html>
