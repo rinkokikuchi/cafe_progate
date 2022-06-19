a8d6ed29375711b71711f3cb024033ec0bf87b4b
rinkokikuchi <raito0903a@gmail.com>
2022-06-19 20:13:25 +0900

    注文画面完成

--- order.php
+++ order.php
@@ -4,29 +4,38 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="order.css">
     <title>注文内容確認</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 </head>
     <body>
-    <div class = "all">
-        <h1>注文内容確認</h1>
-        <div class = "order">
-            <p class = "menu">JUICE×0個</p>
-            <p class= "price">0円</p>
+        <div class="all-container">
+            <div class = "all">
+                <div class = "order-title">
+                    <h1>注文内容確認</h1>
+                </div>
+                <div class = order-container>
+                    <div class = "order">
+                        <p class = "menu">JUICE×0個</p>
+                        <p class= "price">0円</p>
+                    </div>
+                    <div class = "order">
+                        <p class = "menu">COFFEE×0個</p>
+                        <p class= "price">0円</p>
+                    </div>
+                    <div class = "order">
+                        <p class = "menu">CURRY×0個</p>
+                        <p class= "price">0円</p>
+                    </div>
+                    <div class = "order">
+                        <p class = "menu">PASTA×0個</p>
+                        <p class= "price">0円</p>
+                    </div>
+                    </div>
+                <div class = "sum_price">
+                    <h1>合計金額: 0円</h1>
+                </div>
+            </div>
         </div>
-        <div class = "order">
-            <p class = "menu">COFFEE×0個</p>
-            <p class= "price">0円</p>
-        </div>
-        <div class = "order">
-            <p class = "menu">CURRY×0個</p>
-            <p class= "price">0円</p>
-        </div>
-        <div class = "order">
-            <p class = "menu">PASTA×0個</p>
-            <p class= "price">0円</p>
-        </div>
-        <div class = "sum_price">
-            <h1>合計金額: 0円</h1>
-        </div>
-    </div>
     </body>
 </html>
